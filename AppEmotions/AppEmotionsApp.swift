732178c6947a1330d48af4b9027159eeafa5559e
Hami <cala_ham12@hotmail.com>
2026-03-05 14:35:36 +0100

    Initial Commit

---
+++ AppEmotions/AppEmotionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppEmotionsApp.swift
+//  AppEmotions
+//
+//  Created by Equipo 5 on 5/3/26.
+//
+
+import SwiftUI
+
+@main
+struct AppEmotionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
