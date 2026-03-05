a6ea5e61edf1325622d4413bb953ba54fe2bd265
Hami <cala_ham12@hotmail.com>
2026-03-05 14:47:55 +0100

    Inicio del proyecto en firebase

--- AppEmotions/AppEmotionsApp.swift
+++ AppEmotions/AppEmotionsApp.swift
@@ -6,9 +6,24 @@
 //
 
 import SwiftUI
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+      
+    print("🔥 Firebase configurado correctamente")
+
+    return true
+  }
+}
 
 @main
 struct AppEmotionsApp: App {
+    // register app delegate for Firebase setup
+      @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+
     var body: some Scene {
         WindowGroup {
             ContentView()
