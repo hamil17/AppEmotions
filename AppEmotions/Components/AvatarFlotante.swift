2bcd4fdbc00c1105af4f79f478ee901655463954
Hami <cala_ham12@hotmail.com>
2026-03-19 14:24:26 +0100

    Fix: usar avatar_3 cuando todas las tareas completadas (no avatar_4)

--- AppEmotions/Components/AvatarFlotante.swift
+++ AppEmotions/Components/AvatarFlotante.swift
@@ -23,7 +23,7 @@ struct AvatarFlotante: View {
         case 1: return 1
         case 2: return 2
         case 3: return 3
-        default: return 4
+        default: return 3
         }
     }
     
